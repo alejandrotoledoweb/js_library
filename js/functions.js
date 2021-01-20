97b9e55a15526c9e4dded7e3c9bdd9912902b8b6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-01-20 16:49:45 -0500

    exported the necesary functions

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,18 @@
+let myLibrary = [];
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#readStatus');
+const row = document.querySelector('.row');
+const modal = document.querySelector('.modal1');
+const closeButton = document.querySelector('.close-button');
+
 function saveLibrary() {
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+
+export function Card(book) {
+  this.book = book;
 }
 
 // the same attributes from the form in the html file
@@ -11,14 +24,14 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function deleteBook(mybook) {
+export function deleteBook(mybook) {
   const bookIndex = myLibrary.indexOf(mybook.target);
   myLibrary.splice(bookIndex, 1);
   saveLibrary();
   mybook.target.offsetParent.parentElement.remove();
 }
 
-function statusChange(status) {
+export function statusChange(status) {
   if (status.target.textContent === 'Read') {
     status.target.textContent = 'Not Read';
   } else {
@@ -26,7 +39,7 @@ function statusChange(status) {
   }
 }
 
-function checkStatus(book, btn) {
+export function checkStatus(book, btn) {
   if (book.read) {
     btn.textContent = 'Read';
   } else {
@@ -34,7 +47,7 @@ function checkStatus(book, btn) {
   }
 }
 
-function toggleModal() {
+export function toggleModal() {
   modal.classList.toggle('show-modal');
 }
 
@@ -46,7 +59,6 @@ function windowOnClick(event) {
 
 closeButton.addEventListener('click', toggleModal);
 window.addEventListener('click', windowOnClick);
-
 
 function resetList() {
   row.innerHTML = '';
